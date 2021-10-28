7294229d47e8837117c42cd9db5dfe20719b6a15
Orololuwa <67964734+Orololuwa@users.noreply.github.com>
2021-10-28 10:52:33 +0100

    mobile header menu

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,18 +1,25 @@
 import Nav from 'components/Nav';
 import { HeaderContext } from './styled';
 import Button from 'components/Buttons';
+import { CgMenuRight } from 'react-icons/cg';
 
 const Header = props => {
   return (
     <HeaderContext>
       <img src="/logo.png" className="logo" alt="" />
-      <Nav />
-      <div className="button-group">
-        <Button type="ghost" color="primary" _backgroundColor="primary">
-          Sign In
-        </Button>
-        <Button>Get Started</Button>
-      </div>
+      {window.innerWidth > 756 ? (
+        <>
+          <Nav />
+          <div className="button-group">
+            <Button type="ghost" color="primary" _backgroundColor="primary">
+              Sign In
+            </Button>
+            <Button>Get Started</Button>
+          </div>
+        </>
+      ) : (
+        <CgMenuRight />
+      )}
     </HeaderContext>
   );
 };
